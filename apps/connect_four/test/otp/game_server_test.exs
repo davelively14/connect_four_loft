4955047bb68b994ec10a54c83b985d0285f97d9c
Dave Lively <davelively14@gmail.com>
2017-12-07 21:18:08 -0500

    Confirms sizes of default MapSets

--- apps/connect_four/test/otp/game_server_test.exs
+++ apps/connect_four/test/otp/game_server_test.exs
@@ -2,13 +2,18 @@ defmodule ConnectFour.GameServerTest do
   use ExUnit.Case
   alias ConnectFour.GameServer
 
+  setup do
+    ConnectFour.start(nil, %{height: 6, width: 7})
+    :ok
+  end
+
   describe "get_state/0" do
     test "returns state with default params" do
       state = GameServer.get_state()
       assert is_map state.board
-      assert MapSet.size state.board.free
-      assert MapSet.size state.board.player_1
-      assert MapSet.size state.board.player_2
+      assert 7 * 6 == MapSet.size state.board.free
+      assert 0 == MapSet.size state.board.player_1
+      assert 0 == MapSet.size state.board.player_2
 
       assert is_integer state.current_player
       assert is_integer state.height
