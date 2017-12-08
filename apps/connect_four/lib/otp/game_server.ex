b072642f7d4005d74b5eec81916ab5186d44c66f
Dave Lively <davelively14@gmail.com>
2017-12-08 16:53:01 -0500

    Handles a player's turn
    Added drop_piece(column) function. Uses private functions find_open, to
    determine next open spot for a column, and make_move to update the
    state boards

--- apps/connect_four/lib/otp/game_server.ex
+++ apps/connect_four/lib/otp/game_server.ex
@@ -13,6 +13,10 @@ defmodule ConnectFour.GameServer do
     GenServer.call(__MODULE__, :get_state)
   end
 
+  def drop_piece(col) do
+    GenServer.call(__MODULE__, {:drop_piece, col})
+  end
+
   #############
   # Callbacks #
   #############
@@ -23,6 +27,17 @@ defmodule ConnectFour.GameServer do
 
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
+  end
+
+  def handle_call({:drop_piece, col}, _from, state) do
+    if open_spot = find_open(state.board.free, state.height, col) do
+      new_state =
+        make_move(open_spot, state)
+        |> Map.merge(%{current_player: advance_player(state.current_player)})
+      {:reply, :ok, new_state}
+    else
+      {:reply, {:error, "Row is full"}, state}
+    end
   end
 
   #####################
@@ -38,16 +53,38 @@ defmodule ConnectFour.GameServer do
       },
       height: height,
       width: width,
-      current_player: 1,
+      current_player: :player_1,
       finished: nil
     }
   end
 
-  def setup_board(width, height) do
+  defp setup_board(width, height) do
     for x <- 1..width do
       for y <- 1..height do
         {x, y}
       end
     end |> List.flatten |> Enum.into(MapSet.new())
   end
+
+  defp find_open(free, height, col), do: _find_open(free, height, {col, 1})
+  defp _find_open(_, height, {_, row}) when row > height, do: nil
+  defp _find_open(free, height, elem = {col, row}) do
+    cond do
+      MapSet.member?(free, elem) -> elem
+      true -> _find_open(free, height, {col, row + 1})
+    end
+  end
+
+  # Returns state
+  defp make_move(loc, state) do
+    new_free = MapSet.delete(state.board.free, loc)
+    new_player_boar = MapSet.put(state.board[state.current_player], loc)
+    new_board =
+      Map.merge(state.board, Map.new([{state.current_player, new_player_boar}]))
+      |> Map.merge(%{free: new_free})
+    Map.merge(state, %{board: new_board})
+  end
+
+  defp advance_player(:player_1), do: :player_2
+  defp advance_player(:player_2), do: :player_1
 end
