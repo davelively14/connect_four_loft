3f1ce27899b498e72cdd2449958e3931d4241636
Dave Lively <davelively14@gmail.com>
2017-12-08 21:21:03 -0500

    Added check_diag_back
    Added supporting private functions check_up_left and check_down_right

--- apps/connect_four/lib/otp/game_server.ex
+++ apps/connect_four/lib/otp/game_server.ex
@@ -103,7 +103,8 @@ defmodule ConnectFour.GameServer do
   defp check_win(player, player_board, loc) do
     cond do
       check_lateral(player_board, loc) -> player
-      # check_vertical(player_board, loc) -> player
+      check_vertical(player_board, loc) -> player
+      check_diag_back(player_board, loc) -> player
       true -> nil
     end
   end
@@ -113,13 +114,17 @@ defmodule ConnectFour.GameServer do
   end
 
   def check_vertical(player_board, {x, y}), do: check_vertical(1, player_board, {x, y - 1})
-  def check_vertical(4, _, _), do: true
-  def check_vertical(streak, player_board, loc = {x, y}) do
+  defp check_vertical(4, _, _), do: true
+  defp check_vertical(streak, player_board, loc = {x, y}) do
     if MapSet.member?(player_board, loc) do
       check_vertical(streak + 1, player_board, {x, y - 1})
     else
       false
     end
+  end
+
+  def check_diag_back(player_board, {x, y}) do
+    check_up_left(1, player_board, {x - 1, y + 1}) |> check_down_right(player_board, {x + 1, y - 1}) == 4
   end
 
   defp check_left(4, _, _), do: 4
@@ -135,6 +140,24 @@ defmodule ConnectFour.GameServer do
   defp check_right(streak, player_board, loc = {x, y}) do
     if MapSet.member?(player_board, loc) do
       check_right(streak + 1, player_board, {x + 1, y})
+    else
+      streak
+    end
+  end
+
+  defp check_up_left(4, _, _), do: 4
+  defp check_up_left(streak, player_board, loc = {x, y}) do
+    if MapSet.member?(player_board, loc) do
+      check_up_left(streak + 1, player_board, {x - 1, y + 1})
+    else
+      streak
+    end
+  end
+
+  defp check_down_right(4, _, _), do: 4
+  defp check_down_right(streak, player_board, loc = {x, y}) do
+    if MapSet.member?(player_board, loc) do
+      check_down_right(streak + 1, player_board, {x + 1, y - 1})
     else
       streak
     end
