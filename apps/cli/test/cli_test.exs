bfdf0783980daca00d597f61026685ba96c292d5
Dave Lively <davelively14@gmail.com>
2017-12-10 11:41:12 -0500

    Tests that the GameServer starts and is running

--- apps/cli/test/cli_test.exs
+++ apps/cli/test/cli_test.exs
@@ -4,12 +4,10 @@ defmodule CLITest do
   describe "start/2" do
     test "will start workers" do
       assert :erlang.whereis ConnectFour.Server == :undefined
-      assert :erlang.whereis CLI.Server == :undefined
 
       CLI.start(nil, nil)
 
       assert is_pid :erlang.whereis ConnectFour.GameServer
-      assert is_pid :erlang.whereis CLI.Server
     end
   end
 end
