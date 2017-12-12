06c0eb29779717b8891d4cbf6eab226e9a7e7a0d
Dave Lively <davelively14@gmail.com>
2017-12-11 19:21:17 -0500

    Ensures that the GameServer starts only

--- apps/cli/test/cli_test.exs
+++ apps/cli/test/cli_test.exs
@@ -1,11 +1,15 @@
 defmodule CLITest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
 
   describe "start/2" do
     test "will start workers" do
+      func = fn () ->
+        CLI.start(nil, nil)
+      end
       assert :erlang.whereis ConnectFour.Server == :undefined
 
-      CLI.start(nil, nil)
+      assert capture_io("Q", func)
 
       assert is_pid :erlang.whereis ConnectFour.GameServer
     end
