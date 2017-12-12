fff22ce04586c19561e7a5a40cb4fb8c30012161
Dave Lively <davelively14@gmail.com>
2017-12-11 22:05:12 -0500

    Added select({:play_game, 2, state})
    Plays the game. No visual representation.

--- apps/cli/lib/otp/server.ex
+++ apps/cli/lib/otp/server.ex
@@ -45,12 +45,45 @@ defmodule CLI.Server do
     player_1 = IO.gets "Enter first player's name: "
     player_2 = IO.gets "Enter second player's name: "
     select({:play_game, 2, %{player_1: player_1, player_2: player_2}})
+    IO.puts "Let's play!\nEnter 'q' at any time to quit the game."
   end
 
   def select({:play_game, 2, state}) do
-    IO.puts "Let's play!"
-    IO.puts "Enter 'q' at any time to quit the game."
-    IO.gets "#{state[GameServer.current_player]}'s turn: "
+    game_state = GameServer.get_state
+    options = prep_options(game_state.avail_cols)
+
+    IO.puts "Available columns: #{Enum.join(game_state.avail_cols, ", ")}"
+    selection = IO.gets "#{state[game_state.current_player]}'s turn. Select a column: "
+
+    selection = selection |> sanitize_selection
+
+    if is_valid?(selection, options) do
+      if selection == "Q" do
+        exit_game()
+      else
+        result = GameServer.drop_piece(String.to_integer(selection))
+
+        case result do
+          :ok ->
+            IO.puts "Added a piece to column #{selection}"
+            select({:play_game, 2, state})
+          {:ok, :draw} ->
+            print_result(:draw)
+            select(:main_menu)
+          {:ok, winner} ->
+            print_result(state[winner])
+            select(:main_menu)
+          {:error, message} ->
+            IO.puts "#{message}\n\n"
+            select({:play_game, 2, state})
+          _ ->
+            IO.puts "Unknown error, restarting"
+            select(:main_menu)
+        end
+      end
+    else
+      invalid({:play_game, 2, state})
+    end
   end
 
   ########################
@@ -77,4 +110,11 @@ defmodule CLI.Server do
   defp exit_game do
     IO.puts "Thanks for playing!"
   end
+
+  defp prep_options(options), do: prep_options(options |> Enum.map(&(&1)), [])
+  defp prep_options([], result), do: ["Q" | result]
+  defp prep_options([head | tail], result), do: prep_options(tail, [to_string(head) | result])
+
+  defp print_result(:draw), do: IO.puts "The game has ended in a draw! Try again...\n\n"
+  defp print_result(winner), do: IO.puts "Congrats! #{winner} has won the game!\n\n"
 end
