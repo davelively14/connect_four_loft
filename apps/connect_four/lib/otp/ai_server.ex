6ddb7f5b2b7a1a8b6344b47fdd200d4e0701f17d
Dave Lively <davelively14@gmail.com>
2017-12-14 18:43:32 -0500

    Moving checks to separate module

--- apps/connect_four/lib/otp/ai_server.ex
+++ apps/connect_four/lib/otp/ai_server.ex
@@ -1,5 +1,6 @@
 defmodule ConnectFour.AIServer do
   use GenServer
+  alias ConnectFour.GameServer
 
   #######
   # API #
@@ -19,6 +20,10 @@ defmodule ConnectFour.AIServer do
 
   def reset_ai do
     GenServer.call(__MODULE__, :reset_ai)
+  end
+
+  def make_move do
+    GenServer.call(__MODULE__, :make_move)
   end
 
   #############
@@ -45,6 +50,14 @@ defmodule ConnectFour.AIServer do
     {:reply, :ok, initial_state(state.initial_difficulty)}
   end
 
+  def handle_call(:make_move, _from, state) do
+    GameServer.get_state
+    |> select_column(state.difficulty)
+    |> GameServer.drop_piece
+
+    {:reply, :ok, state}
+  end
+
   #####################
   # Support Functions #
   #####################
@@ -55,4 +68,77 @@ defmodule ConnectFour.AIServer do
       difficulty: difficulty
     }
   end
+
+  def select_column(game_state, :easy) do
+    if game_state.last_play do
+      if block = block_opponent?(game_state), do: block, else: Enum.random(game_state.avail_cols)
+    else
+      div(game_state.width + 1, 2)
+    end
+  end
+
+  def select_column(game_state, :hard) do
+    if game_state.last_play do
+      cond do
+        win = win_avail?(game_state) -> win
+        block = block_opponent?(game_state) -> block
+        true -> best_move(game_state)
+      end
+    else
+      div(game_state.width + 1, 2)
+    end
+  end
+
+  def block_opponent?(%{width: width, board: board, last_play: {opponent, last_move}}) do
+    cond do
+      vert = check_vert(board[opponent], last_move, 3) ->
+        vert
+      [left, right] = check_lat(width, board[opponent], last_move, 3) ->
+        cond do
+          MapSet.member?(board.free, left) ->
+            left |> elem(0)
+          MapSet.member?(board.free, right) ->
+            right |> elem(0)
+          true ->
+            false
+        end
+      true -> false
+    end
+    # Check status for 3 in a row
+  end
+
+  def win_avail?(game_state) do
+    # Do I have three in a row?
+  end
+
+  def best_move(game_state) do
+    # Make best move avail
+  end
+
+  ################
+  # Check Status #
+  ################
+
+  defp check_vert(player_board, {x, y}, distance), do: check_vert(1, player_board, {x, y - 1}, distance)
+  defp check_vert(streak, _, {x, _}, distance) when streak == distance, do: x
+  defp check_vert(streak, player_board, loc = {x, y}, distance) do
+    if MapSet.member?(player_board, loc) do
+      check_vert(streak + 1, player_board, {x, y - 1}, distance)
+    else
+      false
+    end
+  end
+
+  def check_lat(width, player_board, {x, y}, distance) do
+    # left = check_left(1, player_board, {x - 1, y}, distance)
+    # right = check_right(1, player_board, {x - 1, y}, distance)
+    # 
+    # if left + right == distance do
+    #   [{x - left, y}, {x + right, y}]
+    # else
+    #   false
+    # end
+  end
+
+  defp check
 end
