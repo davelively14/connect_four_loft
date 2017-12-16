f71b8576449f9ecc45427b17c3169be1d5c0c2b8
Dave Lively <davelively14@gmail.com>
2017-12-16 13:11:57 -0500

    Implemented check_vert/2 and check_vert/4

--- apps/connect_four/lib/otp/ai/status_check.ex
+++ apps/connect_four/lib/otp/ai/status_check.ex
@@ -4,23 +4,30 @@ defmodule ConnectFour.StatusCheck do
   """
 
   @doc """
-  Returns column if there is a contiguous number of pieces for a given distance
-  from a given location. Returns nil if
+  Returns true if there is a contiguous number of pieces for the given distance
+  from a given location. Returns false if distance not reached. Can pass the
+  board, player, location, and distance to the check_vert/4 function or, if just
+  checking the most recent move, a valid game_state and distance to
+  check_vert/2.
 
   ## Examples
 
-      iex> check_vert(#MapSet<[{1, 4}, {1, 3}, {1, 2}]>, {1, 4}, 3)
-      1
-      iex> check_vert(#MapSet<[{1, 4}, {1, 3}, {1, 1}]>, {1, 4}, 3)
-      nil
+      iex> check_vert(valid_board, :player_1, {1, 4}, 3)
+      true
+      iex> check_vert(valid_game_state, 3)
+      true
   """
-  def check_vert(player_board, loc, distance), do: check_vert(0, player_board, loc, distance)
-  defp check_vert(streak, _, {x, _}, distance) when streak == distance, do: x
-  defp check_vert(streak, player_board, loc = {x, y}, distance) do
+  def check_vert(state, distance) do
+    {player, loc} = state.last_play
+    _check_vert(0, state.board[player], loc, distance)
+  end
+  def check_vert(board, player, loc, distance), do: _check_vert(0, board[player], loc, distance)
+  defp _check_vert(streak, _, {_, _}, distance) when streak == distance, do: true
+  defp _check_vert(streak, player_board, loc = {x, y}, distance) do
     if MapSet.member?(player_board, loc) do
-      check_vert(streak + 1, player_board, {x, y - 1}, distance)
+      _check_vert(streak + 1, player_board, {x, y - 1}, distance)
     else
-      nil
+      false
     end
   end
 end
