363306a59ea38e64a4a073a1c61756dafc2fc326
Dave Lively <davelively14@gmail.com>
2017-12-16 14:09:35 -0500

    Added block_vert?/3 tests

--- apps/connect_four/test/otp/ai/status_check_test.exs
+++ apps/connect_four/test/otp/ai/status_check_test.exs
@@ -22,32 +22,54 @@ defmodule ConnectFour.StatusCheckTest do
   describe "check_vert/3" do
     @describetag :setup_board_3_vert
 
-    test "returns column when consecutive pieces met", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_vert(board, player, loc, 3) == 1
+    test "returns true when consecutive pieces condition met", %{board: board, player: player, loc: loc} do
+      assert StatusCheck.check_vert(board, player, loc, 3) == true
     end
 
     test "does not return column if not enough consecutive pieces", %{board: board, player: player, loc: loc} do
-      refute StatusCheck.check_vert(board, player, loc, 4)
+      assert StatusCheck.check_vert(board, player, loc, 4) == false
     end
 
     test "returns column if more than necessary consecutive pieces", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_vert(board, player, loc, 2) == 1
+      assert StatusCheck.check_vert(board, player, loc, 2) == true
     end
   end
 
   describe "check_vert/2" do
     @describetag :setup_board_3_vert
 
-    test "returns column when consecutive pieces met", %{game_state: game_state} do
-      assert StatusCheck.check_vert(game_state, 3) == 1
+    test "returns true when consecutive pieces condition met", %{game_state: game_state} do
+      assert StatusCheck.check_vert(game_state, 3) == true
     end
 
-    test "does not return column if not enough consecutive pieces", %{game_state: game_state} do
-      refute StatusCheck.check_vert(game_state, 4)
+    test "returns false if not enough consecutive pieces", %{game_state: game_state} do
+      assert StatusCheck.check_vert(game_state, 4) == false
     end
 
-    test "returns column if more than necessary consecutive pieces", %{game_state: game_state} do
-      assert StatusCheck.check_vert(game_state, 2) == 1
+    test "returns true if more than necessary consecutive pieces", %{game_state: game_state} do
+      assert StatusCheck.check_vert(game_state, 2) == true
+    end
+  end
+
+  describe "block_vert?/3" do
+    @tag :setup_board_3_vert
+    test "will return column if available", %{board: board, player: player, loc: loc} do
+      assert StatusCheck.block_vert?(board, player, loc) == 1
+    end
+
+    test "will return nil if column is already full" do
+      GameServer.drop_piece(1)
+      GameServer.drop_piece(2)
+      GameServer.drop_piece(1)
+      GameServer.drop_piece(1)
+      GameServer.drop_piece(1)
+      GameServer.drop_piece(2)
+      GameServer.drop_piece(1)
+      GameServer.drop_piece(2)
+      GameServer.drop_piece(1)
+
+      %{board: board, last_play: {player, loc}} = GameServer.get_state()
+      assert StatusCheck.block_vert?(board, player, loc) == nil
     end
   end
 
