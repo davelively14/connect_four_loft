cdb3798db112e73ec2055d8df35053b9c5681893
Dave Lively <davelively14@gmail.com>
2017-12-17 12:46:30 -0500

    Added check_left

--- apps/connect_four/lib/otp/ai/status_check.ex
+++ apps/connect_four/lib/otp/ai/status_check.ex
@@ -22,7 +22,7 @@ defmodule ConnectFour.StatusCheck do
     _check_vert(0, state.board[player], loc, distance)
   end
   def check_vert(board, player, loc, distance), do: _check_vert(0, board[player], loc, distance)
-  defp _check_vert(streak, _, {_, _}, distance) when streak == distance, do: true
+  defp _check_vert(streak, _, _, distance) when streak == distance, do: true
   defp _check_vert(streak, player_board, loc = {x, y}, distance) do
     if MapSet.member?(player_board, loc) do
       _check_vert(streak + 1, player_board, {x, y - 1}, distance)
@@ -48,6 +48,37 @@ defmodule ConnectFour.StatusCheck do
       x
     else
       nil
+    end
+  end
+
+  @doc """
+  Checks for consecutive plays for a given player up to a given a distance to
+  the left. Upon reaching the end, will check if a blocking move is available.
+  Returns a tuple with two values, the first for the streak and the second for
+  the column. The column will be nil if no blocking move to the left is
+  available.
+
+  ## Examples
+
+      iex> check_left(valid_board, :player_1, {4, 2}, 3)
+      {1, 3}
+      iex> check_left(valid_bard, :player_1, {1, 2}, 3)
+      {1, nil}
+  """
+  def check_left(board, player, loc, distance), do: _check_left(0, board, player, loc, distance)
+  defp _check_left(streak, board, _, {x, y}, distance) when streak == distance do
+    # Check if blocking move is available
+    if MapSet.member?(board.free, {x - 1, y}) do
+      {streak, x - 1}
+    else
+      {streak, nil}
+    end
+  end
+  defp _check_left(streak, board, player, loc = {x, y}, distance) do
+    if MapSet.member?(board[player], loc) do
+      _check_left(streak + 1, board, player, {x - 1, y}, distance)
+    else
+      if MapSet.member?(board.free, {x - 1, y}), do: {streak, x - 1}, else: {streak, nil}
     end
   end
 end
