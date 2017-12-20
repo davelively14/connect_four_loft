40365ef0e9dde89a84be89a88212a48d982fb1ba
Dave Lively <davelively14@gmail.com>
2017-12-19 20:26:30 -0500

    Changed tack on StatusCheck, added tests in support
    Simplified strategy for this module. Will merely check winning
    conditions for any given point. Will enable checks for blocking or for
    winning. Tests added for check_lateral/2, check_vertical/2,
    check_diag_back/2, check_diag_fwd/2

--- apps/connect_four/test/otp/ai/status_check_test.exs
+++ apps/connect_four/test/otp/ai/status_check_test.exs
@@ -6,94 +6,76 @@ defmodule ConnectFour.StatusCheckTest do
     GameServer.start_link(%{height: 6, width: 7})
 
     cond do
-      context[:setup_board_3_vert] -> setup_board_3_vert()
-      context[:setup_board_3_lat_mid] -> setup_board_3_lat_mid()
-      context[:setup_board_3_lat_left] -> setup_board_3_lat_left()
-      context[:setup_board_3_lat_right] -> setup_board_3_lat_right()
-      context[:setup_board_3_lat_row_2] -> setup_board_3_lat_row_2()
+      context[:setup_board_vert] -> setup_board_vert()
+      context[:setup_board_lat] -> setup_board_lat()
+      context[:setup_board_diag_back] -> setup_board_diag_back()
+      context[:setup_board_diag_fwd] -> setup_board_diag_fwd()
+      context[:setup_board_full] -> setup_board_full()
       true -> nil
     end
 
     game_state = %{board: board} = GameServer.get_state()
-    if game_state.last_play, do: {player, loc} = game_state.last_play
+
+    {player, loc} =
+      if game_state.last_play, do: game_state.last_play, else: {nil, nil}
+
     {:ok, %{game_state: game_state, board: board, player: player, loc: loc}}
   end
 
-  describe "check_vert/3" do
-    @describetag :setup_board_3_vert
+  describe "check_lateral/2" do
+    @describetag :setup_board_lat
 
-    test "returns true when consecutive pieces condition met", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_vert(board, player, loc, 3) == true
+    test "returns true when a player's piece at loc would win laterally", %{board: board, player: player} do
+      assert StatusCheck.check_lateral(board[player], {6,1})
+      assert StatusCheck.check_lateral(board[player], {2,1})
     end
 
-    test "does not return column if not enough consecutive pieces", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_vert(board, player, loc, 4) == false
-    end
-
-    test "returns column if more than necessary consecutive pieces", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_vert(board, player, loc, 2) == true
-    end
-  end
-
-  describe "check_vert/2" do
-    @describetag :setup_board_3_vert
-
-    test "returns true when consecutive pieces condition met", %{game_state: game_state} do
-      assert StatusCheck.check_vert(game_state, 3) == true
-    end
-
-    test "returns false if not enough consecutive pieces", %{game_state: game_state} do
-      assert StatusCheck.check_vert(game_state, 4) == false
-    end
-
-    test "returns true if more than necessary consecutive pieces", %{game_state: game_state} do
-      assert StatusCheck.check_vert(game_state, 2) == true
+    test "returns false when a player's piece at loc would not win laterally", %{board: board, player: player} do
+      refute StatusCheck.check_lateral(board[player], {1,1})
+      refute StatusCheck.check_lateral(board[player], {7,1})
+      refute StatusCheck.check_lateral(board[player], {3,1})
+      refute StatusCheck.check_lateral(board[player], {4,1})
+      refute StatusCheck.check_lateral(board[player], {5,1})
     end
   end
 
-  describe "block_vert?/3" do
-    @tag :setup_board_3_vert
-    test "will return column if available", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.block_vert?(board, player, loc) == 1
+  describe "check_vertical/2" do
+    @describetag :setup_board_vert
+
+    test "returns true when a player's piece at loc would win vertically", %{board: board, player: player} do
+      assert StatusCheck.check_vertical(board[player], {1,4})
     end
 
-    test "will return nil if column is already full" do
-      GameServer.drop_piece(1)
-      GameServer.drop_piece(2)
-      GameServer.drop_piece(1)
-      GameServer.drop_piece(1)
-      GameServer.drop_piece(1)
-      GameServer.drop_piece(2)
-      GameServer.drop_piece(1)
-      GameServer.drop_piece(2)
-      GameServer.drop_piece(1)
-
-      %{board: board, last_play: {player, loc}} = GameServer.get_state()
-      assert StatusCheck.block_vert?(board, player, loc) == nil
+    test "returns false when a player's piece at loc would not win laterally", %{board: board, player: player} do
+      refute StatusCheck.check_vertical(board[player], {1,3})
+      refute StatusCheck.check_vertical(board[player], {2,1})
+      refute StatusCheck.check_vertical(board[player], {6,6})
     end
   end
 
-  describe "check_left/4" do
-    @tag :setup_board_3_lat_mid
-    test "returns correct streak and column for blocking spot", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_left(board, player, loc, 3) == {2, 1}
+  describe "check_diag_back/2" do
+    @describetag :setup_board_diag_back
+
+    test "returns true when a player's piece at loc would win diagonally back", %{board: board, player: player} do
+      assert StatusCheck.check_diag_back(board[player], {1,4})
     end
 
-    @tag :setup_board_3_lat_left
-    test "returns streak and nil column since no block to the left", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_left(board, player, loc, 3) == {3, nil}
+    test "returns false when a player's piece at loc would not win diagonally back", %{board: board, player: player} do
+      refute StatusCheck.check_diag_back(board[player], {2,4})
+      refute StatusCheck.check_diag_back(board[player], {3,3})
+      refute StatusCheck.check_diag_back(board[player], {4,2})
     end
   end
 
-  describe "check_right/4" do
-    @tag :setup_board_3_lat_mid
-    test "returns correct streak and column for blocking spot", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_right(board, player, loc, 3) == {2, 6}
+  describe "check_diag_fwd/2" do
+    @describetag :setup_board_diag_fwd
+
+    test "returns true when a player's piece at loc would win diagonally back", %{board: board, player: player} do
+      assert StatusCheck.check_diag_fwd(board[player], {4,4})
     end
 
-    @tag :setup_board_3_lat_right
-    test "returns streak and nil column for blocking spot", %{board: board, player: player, loc: loc} do
-      assert StatusCheck.check_right(board, player, loc, 3) == {2, 6}
+    test "returns false when a player's piece at loc would not win diagonally back", %{board: board, player: player} do
+      refute StatusCheck.check_diag_fwd(board[player], {3,4})
     end
   end
 
@@ -101,7 +83,7 @@ defmodule ConnectFour.StatusCheckTest do
   # Private Functions #
   #####################
 
-  defp setup_board_3_vert do
+  defp setup_board_vert do
     GameServer.drop_piece(1)
     GameServer.drop_piece(2)
     GameServer.drop_piece(1)
@@ -109,37 +91,57 @@ defmodule ConnectFour.StatusCheckTest do
     GameServer.drop_piece(1)
   end
 
-  defp setup_board_3_lat_mid do
+  defp setup_board_lat do
     GameServer.drop_piece(3)
     GameServer.drop_piece(3)
-    GameServer.drop_piece(5)
-    GameServer.drop_piece(5)
-    GameServer.drop_piece(4)
-  end
-
-  defp setup_board_3_lat_left do
-    GameServer.drop_piece(1)
-    GameServer.drop_piece(1)
-    GameServer.drop_piece(2)
-    GameServer.drop_piece(2)
-    GameServer.drop_piece(3)
-  end
-
-  defp setup_board_3_lat_right do
-    GameServer.drop_piece(7)
-    GameServer.drop_piece(7)
-    GameServer.drop_piece(6)
-    GameServer.drop_piece(6)
-    GameServer.drop_piece(5)
-  end
-
-  defp setup_board_3_lat_row_2 do
-    GameServer.drop_piece(7)
-    GameServer.drop_piece(3)
-    GameServer.drop_piece(3)
-    GameServer.drop_piece(5)
-    GameServer.drop_piece(5)
     GameServer.drop_piece(4)
     GameServer.drop_piece(4)
+    GameServer.drop_piece(5)
+  end
+
+  defp setup_board_diag_back do
+    GameServer.drop_piece(1)
+    GameServer.drop_piece(1)
+    GameServer.drop_piece(1)
+    GameServer.drop_piece(2)
+    GameServer.drop_piece(4)
+    GameServer.drop_piece(2)
+    GameServer.drop_piece(2)
+    GameServer.drop_piece(3)
+    GameServer.drop_piece(3)
+    GameServer.drop_piece(7)
+    GameServer.drop_piece(3)
+  end
+
+  defp setup_board_diag_fwd do
+    GameServer.drop_piece(4)
+    GameServer.drop_piece(4)
+    GameServer.drop_piece(4)
+    GameServer.drop_piece(3)
+    GameServer.drop_piece(1)
+    GameServer.drop_piece(3)
+    GameServer.drop_piece(3)
+    GameServer.drop_piece(2)
+    GameServer.drop_piece(2)
+    GameServer.drop_piece(7)
+    GameServer.drop_piece(2)
+  end
+
+  defp setup_board_full do
+    for outer <- 0..1 do
+      for x <- 1..3 do
+        for _ <- 0..4 do
+          GameServer.drop_piece(x + outer * 3)
+        end
+      end
+      for x <- 1..3 do
+        GameServer.drop_piece(x + outer * 3)
+      end
+    end
+    for _ <- 1..6 do
+      GameServer.drop_piece(7)
+    end
+    |> List.flatten()
+    |> List.last()
   end
 end
