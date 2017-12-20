41505ccbc5815d1673430cae496a54cd3c32b718
Dave Lively <davelively14@gmail.com>
2017-12-19 20:30:39 -0500

    StatusCheck passes tests for new implementation
    Changing strategy. Simply provides checks on winning for a given
    location on the game board. Can be used to determine blocking option or
    if the game is won. Checks were all added.

--- apps/connect_four/lib/otp/ai/status_check.ex
+++ apps/connect_four/lib/otp/ai/status_check.ex
@@ -4,81 +4,103 @@ defmodule ConnectFour.StatusCheck do
   """
 
   @doc """
-  Returns true if there is a contiguous number of pieces for the given distance
-  from a given location. Returns false if distance not reached. Can pass the
-  board, player, location, and distance to the check_vert/4 function or, if just
-  checking the most recent move, a valid game_state and distance to
-  check_vert/2.
+  Returns a list of columns that would produce a win for the opposing player.
 
   ## Examples
 
-      iex> check_vert(valid_board, :player_1, {1, 4}, 3)
-      true
-      iex> check_vert(valid_game_state, 3)
-      true
+      iex> must_block(valid_board, :opposing_player, avail_cols)
+      [1]
+      iex> must_block(valid_board, :opposing_player, avail_cols)
+      []
   """
-  def check_vert(state, distance) do
-    {player, loc} = state.last_play
-    _check_vert(0, state.board[player], loc, distance)
+  # def must_block(board, player, avail_cols) do
+  #   # check
+  # end
+  #
+  # def check_win_or_draw(player, free, player_board, loc) do
+  #   cond do
+  #     check_lateral(player_board, loc) -> player
+  #     check_vertical(player_board, loc) -> player
+  #     check_diag_back(player_board, loc) -> player
+  #     check_diag_fwd(player_board, loc) -> player
+  #     MapSet.size(free) == 0 -> :draw
+  #     true -> nil
+  #   end
+  # end
+
+  def check_lateral(player_board, {x, y}) do
+    check_left(1, player_board, {x - 1, y}) |> check_right(player_board, {x + 1, y}) == 4
   end
-  def check_vert(board, player, loc, distance), do: _check_vert(0, board[player], loc, distance)
-  defp _check_vert(streak, _, _, distance) when streak == distance, do: true
-  defp _check_vert(streak, player_board, loc = {x, y}, distance) do
+
+  def check_vertical(player_board, {x, y}), do: check_vertical(1, player_board, {x, y - 1})
+  defp check_vertical(4, _, _), do: true
+  defp check_vertical(streak, player_board, loc = {x, y}) do
     if MapSet.member?(player_board, loc) do
-      _check_vert(streak + 1, player_board, {x, y - 1}, distance)
+      check_vertical(streak + 1, player_board, {x, y - 1})
     else
       false
     end
   end
 
-  @doc """
-  Returns column to block if needed. If no block needed, returns nil.
+  def check_diag_back(player_board, {x, y}) do
+    check_up_left(1, player_board, {x - 1, y + 1}) |> check_down_right(player_board, {x + 1, y - 1}) == 4
+  end
 
-  ## Examples
+  def check_diag_fwd(player_board, {x, y}) do
+    check_up_right(1, player_board, {x + 1, y + 1}) |> check_down_left(player_board, {x - 1, y - 1}) == 4
+  end
 
-      iex> block_vert?(valid_board, :player_1, {1, 4})
-      1
-      iex> block_vert?(valid_bard, :player_1, {2, 2})
-      nil
-      iex> block_vert?(valid_board, :player_1, {1, 6})
-      nil
-  """
-  def block_vert?(board, player, loc = {x, y}) do
-    if check_vert(board, player, loc, 3) && MapSet.member?(board.free, {x, y + 1}) do
-      x
+  defp check_left(4, _, _), do: 4
+  defp check_left(streak, player_board, loc = {x, y}) do
+    if MapSet.member?(player_board, loc) do
+      check_left(streak + 1, player_board, {x - 1, y})
     else
-      nil
+      streak
     end
   end
 
-  @doc """
-  Checks for consecutive plays for a given player up to a given a distance to
-  the left. Upon reaching the end, will check if a blocking move is available.
-  Returns a tuple with two values, the first for the streak and the second for
-  the column. The column will be nil if no blocking move to the left is
-  available.
-
-  ## Examples
-
-      iex> check_left(valid_board, :player_1, {4, 2}, 3)
-      {1, 3}
-      iex> check_left(valid_bard, :player_1, {1, 2}, 3)
-      {1, nil}
-  """
-  def check_left(board, player, loc, distance), do: _check_left(0, board, player, loc, distance)
-  defp _check_left(streak, board, _, {x, y}, distance) when streak == distance do
-    # Check if blocking move is available
-    if MapSet.member?(board.free, {x - 1, y}) do
-      {streak, x - 1}
+  defp check_right(4, _, _), do: 4
+  defp check_right(streak, player_board, loc = {x, y}) do
+    if MapSet.member?(player_board, loc) do
+      check_right(streak + 1, player_board, {x + 1, y})
     else
-      {streak, nil}
+      streak
     end
   end
-  defp _check_left(streak, board, player, loc = {x, y}, distance) do
-    if MapSet.member?(board[player], loc) do
-      _check_left(streak + 1, board, player, {x - 1, y}, distance)
+
+  defp check_up_left(4, _, _), do: 4
+  defp check_up_left(streak, player_board, loc = {x, y}) do
+    if MapSet.member?(player_board, loc) do
+      check_up_left(streak + 1, player_board, {x - 1, y + 1})
     else
-      if MapSet.member?(board.free, {x - 1, y}), do: {streak, x - 1}, else: {streak, nil}
+      streak
+    end
+  end
+
+  defp check_down_right(4, _, _), do: 4
+  defp check_down_right(streak, player_board, loc = {x, y}) do
+    if MapSet.member?(player_board, loc) do
+      check_down_right(streak + 1, player_board, {x + 1, y - 1})
+    else
+      streak
+    end
+  end
+
+  defp check_up_right(4, _, _), do: 4
+  defp check_up_right(streak, player_board, loc = {x, y}) do
+    if MapSet.member?(player_board, loc) do
+      check_up_right(streak + 1, player_board, {x + 1, y + 1})
+    else
+      streak
+    end
+  end
+
+  defp check_down_left(4, _, _), do: 4
+  defp check_down_left(streak, player_board, loc = {x, y}) do
+    if MapSet.member?(player_board, loc) do
+      check_down_left(streak + 1, player_board, {x - 1, y - 1})
+    else
+      streak
     end
   end
 end
