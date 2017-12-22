97ec54bcec866775b8bbfb9fbca08eb1f6e4dcc5
Dave Lively <davelively14@gmail.com>
2017-12-22 15:08:33 -0500

    Will convert string height and width params to integers

--- apps/connect_four_backend/lib/connect_four_backend_web/controllers/game_controller.ex
+++ apps/connect_four_backend/lib/connect_four_backend_web/controllers/game_controller.ex
@@ -3,6 +3,9 @@ defmodule ConnectFourBackendWeb.GameController do
   alias ConnectFour.GameServer
 
   def create(conn, %{"width" => width, "height" => height}) do
+    height = ensure_int(height)
+    width = ensure_int(width)
+
     if :erlang.whereis(ConnectFour) == :undefined do
       ConnectFour.start(nil, %{height: height, width: width})
     else
@@ -13,4 +16,12 @@ defmodule ConnectFourBackendWeb.GameController do
     render conn, "state.json", game_state
   end
   def create(conn, _), do: create(conn, %{"width" => 7, "height" => 6})
+
+  #####################
+  # Private Functions #
+  #####################
+
+  defp ensure_int(value) do
+    if is_bitstring(value), do: String.to_integer(value), else: value
+  end
 end
