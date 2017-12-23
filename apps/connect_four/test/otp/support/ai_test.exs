fc8c4d99aa7b9b9bf48bd01502bb55b52552135f
Dave Lively <davelively14@gmail.com>
2017-12-22 20:58:08 -0500

    Configured tests for using game_id

--- apps/connect_four/test/otp/support/ai_test.exs
+++ apps/connect_four/test/otp/support/ai_test.exs
@@ -3,29 +3,31 @@ defmodule ConnectFour.AITest do
   alias ConnectFour.{AI, GameServer}
 
   setup do
-    GameServer.start_link(%{height: 6, width: 7})
-    :ok
+    GameServer.start_link()
+    {:ok, game_id} = GameServer.new_game()
+
+    {:ok, %{game_id: game_id}}
   end
 
   describe "select_column(game_state, :easy)" do
-    test "selects center column when nothing has been played yet" do
-      assert AI.select_column(GameServer.get_state, :easy) == 4
+    test "selects center column when nothing has been played yet", %{game_id: game_id} do
+      assert AI.select_column(GameServer.get_game(game_id), :easy) == 4
     end
 
-    test "selects random column when user is not in danger of winning" do
-      GameServer.drop_piece(1)
-      col = AI.select_column(GameServer.get_state, :easy)
+    test "selects random column when user is not in danger of winning", %{game_id: game_id} do
+      GameServer.drop_piece(game_id, 1)
+      col = AI.select_column(GameServer.get_game(game_id), :easy)
       assert col >= 1 && col <= 7
     end
 
-    test "blocks user if in danger of winning" do
-      setup_vert_3()
-      assert AI.select_column(GameServer.get_state, :easy) == 1
+    test "blocks user if in danger of winning", %{game_id: game_id} do
+      setup_vert_3(game_id)
+      assert AI.select_column(GameServer.get_game(game_id), :easy) == 1
 
-      GameServer.reset_game()
+      GameServer.reset_game(game_id)
 
-      setup_lat_3()
-      assert AI.select_column(GameServer.get_state, :easy) == 4
+      setup_lat_3(game_id)
+      assert AI.select_column(GameServer.get_game(game_id), :easy) == 4
     end
   end
 
@@ -34,19 +36,19 @@ defmodule ConnectFour.AITest do
   # Private Functions #
   #####################
 
-  defp setup_vert_3 do
-    GameServer.drop_piece(1)
-    GameServer.drop_piece(2)
-    GameServer.drop_piece(1)
-    GameServer.drop_piece(2)
-    GameServer.drop_piece(1)
+  defp setup_vert_3(game_id) do
+    GameServer.drop_piece(game_id, 1)
+    GameServer.drop_piece(game_id, 2)
+    GameServer.drop_piece(game_id, 1)
+    GameServer.drop_piece(game_id, 2)
+    GameServer.drop_piece(game_id, 1)
   end
 
-  defp setup_lat_3 do
-    GameServer.drop_piece(1)
-    GameServer.drop_piece(1)
-    GameServer.drop_piece(2)
-    GameServer.drop_piece(2)
-    GameServer.drop_piece(3)
+  defp setup_lat_3(game_id) do
+    GameServer.drop_piece(game_id, 1)
+    GameServer.drop_piece(game_id, 1)
+    GameServer.drop_piece(game_id, 2)
+    GameServer.drop_piece(game_id, 2)
+    GameServer.drop_piece(game_id, 3)
   end
 end
