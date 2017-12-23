707b22d506b890e58127a405f986473dcd7e52ed
Dave Lively <davelively14@gmail.com>
2017-12-23 08:39:18 -0500

    GameController now handles params with correct statuses

--- apps/connect_four_backend/lib/connect_four_backend_web/controllers/game_controller.ex
+++ apps/connect_four_backend/lib/connect_four_backend_web/controllers/game_controller.ex
@@ -3,22 +3,24 @@ defmodule ConnectFourBackendWeb.GameController do
   alias ConnectFour.GameServer
 
   def create(conn, %{"width" => width, "height" => height}) do
-    height = ensure_int(height)
-    width = ensure_int(width)
+    height = ensure_positive_int(height)
+    width = ensure_positive_int(width)
 
     if :erlang.whereis(ConnectFour) == :undefined do
       ConnectFour.start()
     end
 
-    {:ok, game_id} =
-      if height && width do
-        GameServer.new_game(height, width)
-      else
-        GameServer.new_game
-      end
-
-    game_state = GameServer.get_game(game_id)
-    render conn, "state.json", %{game_state: game_state, game_id: game_id}
+    if height && width do
+      {:ok, game_id} = GameServer.new_game(height, width)
+      game_state = GameServer.get_game(game_id)
+      conn
+      |> put_status(201)
+      |> render "state.json", %{game_state: game_state, game_id: game_id}
+    else
+      conn
+      |> put_status(422)
+      |> render "error.json", %{error: "Invalid parameters"}
+    end
   end
   def create(conn, _), do: create(conn, %{"width" => 7, "height" => 6})
 
@@ -26,11 +28,11 @@ defmodule ConnectFourBackendWeb.GameController do
   # Private Functions #
   #####################
 
-  defp ensure_int(value) when is_integer(value), do: value
-  defp ensure_int(value) do
+  defp ensure_positive_int(value) when is_integer(value), do: value
+  defp ensure_positive_int(value) do
     case Integer.parse(value) do
       {num, ""} ->
-        num
+        if num > 0, do: num, else: nil
       _ ->
         nil
     end
--- apps/connect_four_backend/test/connect_four_backend_web/controllers/game_controller_test.exs
+++ apps/connect_four_backend/test/connect_four_backend_web/controllers/game_controller_test.exs
@@ -4,7 +4,7 @@ defmodule ConnectFourBackendWeb.GameControllerTest do
   describe "POST create" do
     test "with no parameters, starts a new game with default dimensions", %{conn: conn} do
       conn = post conn, game_path(conn, :create)
-      assert state = json_response(conn, 200)
+      assert state = json_response(conn, 201)
 
       assert state["height"] == 6
       assert state["width"] == 7
@@ -13,17 +13,26 @@ defmodule ConnectFourBackendWeb.GameControllerTest do
 
     test "with parameters, will start a new game with specified dimensions", %{conn: conn} do
       conn = post conn, game_path(conn, :create), height: 2, width: 10
-      assert state = json_response(conn, 200)
+      assert state = json_response(conn, 201)
 
       assert state["height"] == 2
       assert state["width"] == 10
       assert state["board"]["free"] |> length == 2 * 10
     end
 
-    @tag :current
-    test "with incorrect parameters, will produce error", %{conn: conn} do
+    test "with incorrect parameters, will start a game with default parameters", %{conn: conn} do
       conn = post conn, game_path(conn, :create), height: "hello", width: "world"
-      assert state = json_response(conn, 200)
+      assert %{"errors" => "Invalid parameters"} == json_response(conn, 422)
+    end
+
+    test "with negative height, will start a game with default parameters", %{conn: conn} do
+      conn = post conn, game_path(conn, :create), height: "-4", width: "5"
+      assert %{"errors" => "Invalid parameters"} == json_response(conn, 422)
+    end
+
+    test "with negative width, will start a game with default parameters", %{conn: conn} do
+      conn = post conn, game_path(conn, :create), height: "4", width: "-5"
+      assert %{"errors" => "Invalid parameters"} == json_response(conn, 422)
     end
   end
 end
