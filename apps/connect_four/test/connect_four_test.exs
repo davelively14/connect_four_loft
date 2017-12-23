2aac4c022c84b4f2e4575b5376f29ef83d6664b1
Dave Lively <davelively14@gmail.com>
2017-12-22 20:33:01 -0500

    Removed unnecessary tests

--- apps/connect_four/test/connect_four_test.exs
+++ apps/connect_four/test/connect_four_test.exs
@@ -5,7 +5,7 @@ defmodule ConnectFourTest do
   describe "start/2" do
     test "will start workers" do
       start = fn () ->
-        ConnectFour.start(nil)
+        ConnectFour.start()
       end
 
       assert :erlang.whereis ConnectFour.GameServer == :undefined
@@ -13,21 +13,6 @@ defmodule ConnectFourTest do
       assert capture_io("Q", start)
 
       assert is_pid :erlang.whereis ConnectFour.GameServer
-    end
-
-    test "will set default width and height if non provided" do
-      assert ConnectFour.start(nil)
-    end
-
-    test "will accept a different width and height" do
-      assert ConnectFour.start(nil, %{width: 10, height: 10})
-    end
-
-    test "will raise error with invalid params" do
-      assert_raise FunctionClauseError, fn -> ConnectFour.start(nil, %{}) end
-      assert_raise FunctionClauseError, fn -> ConnectFour.start(nil, %{height: 12}) end
-      assert_raise FunctionClauseError, fn -> ConnectFour.start(nil, %{width: 12}) end
-      assert_raise FunctionClauseError, fn -> ConnectFour.start(nil, %{width: 12, kite: 10}) end
     end
   end
 end
