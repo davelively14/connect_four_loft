ff4b6cf834e55e2c32de21363ba0298393f09df0
Dave Lively <davelively14@gmail.com>
2017-12-26 12:05:18 -0500

    Added link to new-game

--- apps/connect_four_backend/lib/connect_four_backend_web/static/js/components/landing.js
+++ apps/connect_four_backend/lib/connect_four_backend_web/static/js/components/landing.js
@@ -3,6 +3,8 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 // import * as ActionCreators from '../actions/index';
 
+import { Link } from 'react-router';
+
 const mapStateToProps = function(_state) {
   return {
 
@@ -22,41 +24,13 @@ class Landing extends Component {
       <div>
         <div className="jumbotron">
           <h2>Welcome to Connect Four</h2>
-          <p className="lead">A productive web framework that<br />does not compromise speed and maintainability.</p>
-        </div>
-
-        <div className="row marketing">
-          <div className="col-lg-6">
-            <h4>Resources</h4>
-            <ul>
-              <li>
-                <a href="http://phoenixframework.org/docs/overview">Guides</a>
-              </li>
-              <li>
-                <a href="https://hexdocs.pm/phoenix">Docs</a>
-              </li>
-              <li>
-                <a href="https://github.com/phoenixframework/phoenix">Source</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-lg-6">
-            <h4>Help</h4>
-            <ul>
-              <li>
-                <a href="http://groups.google.com/group/phoenix-talk">Mailing list</a>
-              </li>
-              <li>
-                <a href="http://webchat.freenode.net/?channels=elixir-lang">#elixir-lang on freenode IRC</a>
-              </li>
-              <li>
-                <a href="https://twitter.com/elixirphoenix">@elixirphoenix</a>
-              </li>
-            </ul>
+          <p className="lead">My test project for <a href="https://salesloft.com/" target="_blank">SalesLoft</a></p>
+          <div className="row">
+            <Link to="new-game">
+              <button className="btn btn-primary">New Game</button>
+            </Link>
           </div>
         </div>
-
       </div>
     );
   }
