a91d88f2a90b0f45ea7ef0c577f3bcbd04ea4f5d
Dave Lively <davelively14@gmail.com>
2017-12-26 10:32:47 -0500

    Made formatting consistent

--- apps/connect_four_backend/lib/connect_four_backend_web/static/js/phoenix_html.js
+++ apps/connect_four_backend/lib/connect_four_backend_web/static/js/phoenix_html.js
@@ -5,11 +5,11 @@ var len = elements.length;
 
 for (var i = 0; i < len; ++i) {
   elements[i].addEventListener('click', function (event) {
-    var message = this.getAttribute("data-confirm");
+    var message = this.getAttribute('data-confirm');
     if (message === null || confirm(message)) {
       this.parentNode.submit();
-    };
+    }
     event.preventDefault();
     return false;
   }, false);
-};
+}
