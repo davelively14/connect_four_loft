73117c2c3c467c7bebcacee15375e9f6a2842c0f
Dave Lively <davelively14@gmail.com>
2017-12-26 11:26:50 -0500

    Hooked up router and store, added app and landing components

--- apps/connect_four_backend/lib/connect_four_backend_web/static/js/react.js
+++ apps/connect_four_backend/lib/connect_four_backend_web/static/js/react.js
@@ -1,45 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+
+import connectFourApp from './reducers/index';
+import App from './components/app';
+import Landing from './components/landing';
+
+let store = createStore(connectFourApp);
+let history = syncHistoryWithStore(browserHistory, store);
 
 render(
-  <div className="container">
-    <div className="jumbotron">
-      <h2>Welcome to Phoenix</h2>
-      <p className="lead">A productive web framework that<br />does not compromise speed and maintainability.</p>
-    </div>
-    
-    <div className="row marketing">
-      <div className="col-lg-6">
-        <h4>Resources</h4>
-        <ul>
-          <li>
-            <a href="http://phoenixframework.org/docs/overview">Guides</a>
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix">Source</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="col-lg-6">
-        <h4>Help</h4>
-        <ul>
-          <li>
-            <a href="http://groups.google.com/group/phoenix-talk">Mailing list</a>
-          </li>
-          <li>
-            <a href="http://webchat.freenode.net/?channels=elixir-lang">#elixir-lang on freenode IRC</a>
-          </li>
-          <li>
-            <a href="https://twitter.com/elixirphoenix">@elixirphoenix</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-  </div>,
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Landing} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('react')
 );
