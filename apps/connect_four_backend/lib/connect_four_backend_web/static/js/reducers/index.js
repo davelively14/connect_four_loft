9939281c9b7ebb4b0da7e52cd871e3b8f340babc
Dave Lively <davelively14@gmail.com>
2017-12-26 15:54:31 -0500

    Added game reducer

--- apps/connect_four_backend/lib/connect_four_backend_web/static/js/reducers/index.js
+++ apps/connect_four_backend/lib/connect_four_backend_web/static/js/reducers/index.js
@@ -2,7 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
 
+import game from './game';
+
 const connectFourFrontendApp = combineReducers({
+  game,
   routing: routerReducer,
   form: formReducer
 });
