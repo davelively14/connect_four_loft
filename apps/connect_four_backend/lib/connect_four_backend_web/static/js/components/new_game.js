32fe3288aa06511796dca82bafabd7599b0fefb5
Dave Lively <davelively14@gmail.com>
2017-12-26 21:50:49 -0500

    Created basic NewGame component
    Next up, plug in the API.

--- apps/connect_four_backend/lib/connect_four_backend_web/static/js/components/new_game.js
+++ apps/connect_four_backend/lib/connect_four_backend_web/static/js/components/new_game.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import * as ActionCreators from '../actions/index';
+import { SubmissionError } from 'redux-form';
+import { browserHistory } from 'react-router';
+import { url } from '../helpers/api';
+
+import NewGameForm from './forms/new_game_form';
 
 const mapStateToProps = function(_state) {
   return {
@@ -8,14 +14,43 @@ const mapStateToProps = function(_state) {
   };
 };
 
+const mapDispatchToProps = function (dispatch) {
+  return bindActionCreators(ActionCreators, dispatch);
+};
+
 class NewGame extends Component {
+  submit(values) {
+    switch (true) {
+      case values.player1 == undefined:
+        throw new SubmissionError({
+          player1: 'Player 1 name cannot be blank',
+          _error: 'Invalid name'
+        });
+      case values.player2 == undefined && values.difficulty == undefined:
+        throw new SubmissionError({
+          player2: 'Player 2 name cannot be blank',
+          _error: 'Invalid name'
+        });
+      default:
+        console.log(values);
+        console.log(url());
+        browserHistory.push('/');
+    }
+  }
+
   render() {
     return(
-      <div>
-        <h2>New Game</h2>
+      <div className="row">
+        <div className="col-sm-3" />
+        <div className="col-sm-6">
+          <h2>New Game</h2>
+          <div className="form-group">
+            <NewGameForm onSubmit={this.submit.bind(this)} />
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps, {})(NewGame);
+export default connect(mapStateToProps, mapDispatchToProps)(NewGame);
