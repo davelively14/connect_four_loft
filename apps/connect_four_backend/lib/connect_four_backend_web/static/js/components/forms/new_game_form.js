6e9da8c7ee5177a70edc870a012d780c14496982
Dave Lively <davelively14@gmail.com>
2017-12-26 21:50:00 -0500

    Using the correct component for autoFocus

--- apps/connect_four_backend/lib/connect_four_backend_web/static/js/components/forms/new_game_form.js
+++ apps/connect_four_backend/lib/connect_four_backend_web/static/js/components/forms/new_game_form.js
@@ -9,7 +9,7 @@ class NewGameForm extends Component {
 
     return(
       <form onSubmit={handleSubmit}>
-        <Field name="player1" type="text" component={renderFieldAndLabel} placeholder="First Player Name" autoFocus />
+        <Field name="player1" type="text" component={renderFieldAndLabelFocus} placeholder="First Player Name" />
         <Field name="player2" type="text" component={renderFieldAndLabel} placeholder="Second Player Name" />
 
         <button type="submit" className="btn btn-primary">Submit</button>
