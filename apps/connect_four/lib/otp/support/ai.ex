592c4cabae600e1e6ac9d517cce01b1cf2ca6eec
Dave Lively <davelively14@gmail.com>
2017-12-28 15:22:52 -0500

    Added scaffolding for :hard

--- apps/connect_four/lib/otp/support/ai.ex
+++ apps/connect_four/lib/otp/support/ai.ex
@@ -7,14 +7,31 @@ defmodule ConnectFour.AI do
 
   def select_column(game_state, :easy) do
     if game_state.last_play do
-      block = StatusCheck.get_block_cols(game_state)
-      if length(block) > 0 do
-        Enum.random(block)
+      if block = StatusCheck.get_block_cols(game_state) do
+        block
       else
         Enum.random(game_state.avail_cols)
       end
     else
       div(game_state.width + 1, 2)
+    end
+  end
+
+  # Strategy:
+  # 1. If win available, win
+  # 2. If opponent could win, block
+  # 3. Heuristically select best move available
+  def select_column(game_state, :hard) do
+    cond do
+      col = StatusCheck.get_win_col(game_state) ->
+        col
+      col = StatusCheck.get_block_cols(game_state) ->
+        col
+      !game_state.last_play ->
+        div(game_state.width + 1, 2)
+      true ->
+        # TODO: replace with heuristic solution
+        Enum.random(game_state.avail_cols)
     end
   end
 end
