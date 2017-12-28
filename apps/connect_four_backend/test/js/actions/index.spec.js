92bc4383404df590953f499d1d18991d4765acee
Dave Lively <davelively14@gmail.com>
2017-12-28 10:34:50 -0500

    Moved buildGameState and buildPlayerInfo into a helper file

--- apps/connect_four_backend/test/js/actions/index.spec.js
+++ apps/connect_four_backend/test/js/actions/index.spec.js
@@ -1,4 +1,5 @@
 import * as actions from '../../../lib/connect_four_backend_web/static/js/actions/index';
+import { buildGameState, buildPlayerInfo } from '../helpers/index';
 
 describe('index', () => {
   describe('types', () => {
@@ -30,49 +31,3 @@ describe('index', () => {
     });
   });
 });
-
-/********************
-  SUPPORT FUNCTIONS
-********************/
-
-function buildGameState(opts = {}) {
-  let baseGame = {
-    id: 1,
-    board: {
-      free: [[1, 1], [1, 2], [2, 1], [2, 2]],
-      player_1: [],
-      player_2: []
-    },
-    height: 2,
-    width: 2,
-    last_play: undefined,
-    avail_cols: [1, 2],
-    current_player: 'player_1',
-    finished: undefined,
-    difficulty: undefined
-  };
-
-  if (opts.madeMove) {
-    return Object.assign({},
-      baseGame,
-      {
-        board: {
-          free: [[1, 2], [2, 1], [2, 2]],
-          player_1: [[1, 1]],
-          player_2: []
-        },
-        current_player: 'player_2',
-        last_play: ['player_1', [1, 1]]
-      }
-    );
-  } else {
-    return baseGame;
-  }
-}
-
-function buildPlayerInfo(opts = {}) {
-  return {
-    player_1: opts.player_1 || 'first',
-    player_2: opts.player_2 || 'second'
-  };
-}
