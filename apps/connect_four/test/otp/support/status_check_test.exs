f2a69e07a52c666fff7d445feaf3ee243433645a
Dave Lively <davelively14@gmail.com>
2017-12-28 15:08:01 -0500

    Added get_win_col/1 tests

--- apps/connect_four/test/otp/support/status_check_test.exs
+++ apps/connect_four/test/otp/support/status_check_test.exs
@@ -7,10 +7,14 @@ defmodule ConnectFour.StatusCheckTest do
     {:ok, game_id} = GameServer.new_game()
 
     cond do
-      context[:setup_board_vert] -> setup_board_vert(game_id)
-      context[:setup_board_lat] -> setup_board_lat(game_id)
-      context[:setup_board_diag_back] -> setup_board_diag_back(game_id)
-      context[:setup_board_diag_fwd] -> setup_board_diag_fwd(game_id)
+      context[:setup_board_vert_block] -> setup_board_vert_block(game_id)
+      context[:setup_board_vert_win] -> setup_board_vert_win(game_id)
+      context[:setup_board_lat_block] -> setup_board_lat_block(game_id)
+      context[:setup_board_lat_win] -> setup_board_lat_win(game_id)
+      context[:setup_board_diag_back_block] -> setup_board_diag_back_block(game_id)
+      context[:setup_board_diag_back_win] -> setup_board_diag_back_win(game_id)
+      context[:setup_board_diag_fwd_block] -> setup_board_diag_fwd_block(game_id)
+      context[:setup_board_diag_fwd_win] -> setup_board_diag_fwd_win(game_id)
       context[:setup_board_full] -> setup_board_full(game_id)
       true -> nil
     end
@@ -28,24 +32,24 @@ defmodule ConnectFour.StatusCheckTest do
       refute StatusCheck.get_block_cols(game_state)
     end
 
-    @tag :setup_board_vert
+    @tag :setup_board_vert_block
     test "returns correct column required for blocking vertically", %{game_state: game_state} do
       assert StatusCheck.get_block_cols(game_state) == 1
     end
 
-    @tag :setup_board_lat
+    @tag :setup_board_lat_block
     test "returns multiple columns for blocking when multiple ways to win", %{game_state: game_state} do
       result = StatusCheck.get_block_cols(game_state)
 
       assert result == 2 || result == 6
     end
 
-    @tag :setup_board_diag_back
+    @tag :setup_board_diag_back_block
     test "returns column for blocking a diagonal back", %{game_state: game_state} do
       assert StatusCheck.get_block_cols(game_state) == 1
     end
 
-    @tag :setup_board_diag_fwd
+    @tag :setup_board_diag_fwd_block
     test "returns column for blocking a diagonal forward", %{game_state: game_state} do
       assert StatusCheck.get_block_cols(game_state) == 4
     end
@@ -56,47 +60,80 @@ defmodule ConnectFour.StatusCheckTest do
     end
   end
 
+  describe "get_win_col/1" do
+    test "returns nil if no move to win", %{game_state: game_state} do
+      refute StatusCheck.get_block_cols(game_state)
+    end
+
+    @tag :setup_board_vert_win
+    test "returns column for winning when vertical win available", %{game_state: game_state} do
+      assert StatusCheck.get_win_col(game_state) == 1
+    end
+
+    @tag :setup_board_lat_win
+    test "returns column for winning when lateral win available", %{game_state: game_state} do
+      resp = StatusCheck.get_win_col(game_state)
+
+      assert resp == 2 || resp == 6
+    end
+
+    @tag :setup_board_diag_back_win
+    test "returns column for winning when diagonal back win available", %{game_state: game_state} do
+      assert StatusCheck.get_win_col(game_state) == 1
+    end
+
+    @tag :setup_board_diag_fwd_win
+    test "returns column for winning when diagonal fwd win available", %{game_state: game_state} do
+      assert StatusCheck.get_win_col(game_state) == 4
+    end
+
+    @tag :setup_board_full
+    test "returns nil if board is full", %{game_state: game_state} do
+      refute StatusCheck.get_win_col(game_state)
+    end
+  end
+
   describe "check_win_or_draw/3" do
-    @tag :setup_board_vert
+    @tag :setup_board_vert_block
     test "returns player when a player's piece at loc would win vertically", %{board: board, player: player} do
       assert StatusCheck.check_win_or_draw(board, player, {1,4}) == player
     end
 
-    @tag :setup_board_vert
+    @tag :setup_board_vert_block
     test "returns nil when a player's piece at loc would not win vertically", %{board: board, player: player} do
       assert StatusCheck.check_win_or_draw(board, player, {1,3}) == nil
     end
 
-    @tag :setup_board_lat
+    @tag :setup_board_lat_block
     test "returns player when a player's piece at loc would win laterally", %{board: board, player: player} do
       assert StatusCheck.check_win_or_draw(board, player, {6,1}) == player
       assert StatusCheck.check_win_or_draw(board, player, {2,1}) == player
     end
 
-    @tag :setup_board_lat
+    @tag :setup_board_lat_block
     test "returns nil when a player's piece at loc would not win laterally", %{board: board, player: player} do
       assert StatusCheck.check_win_or_draw(board, player, {1,1}) == nil
       assert StatusCheck.check_win_or_draw(board, player, {7,1}) == nil
       assert StatusCheck.check_win_or_draw(board, player, {3,1}) == nil
     end
 
-    @tag :setup_board_diag_back
+    @tag :setup_board_diag_back_block
     test "returns player when a player's piece at loc would win diagonally backward", %{board: board, player: player} do
       assert StatusCheck.check_win_or_draw(board, player, {1,4}) == player
     end
 
-    @tag :setup_board_diag_back
+    @tag :setup_board_diag_back_block
     test "returns nil when a player's piece at loc would not win diagonally backward", %{board: board, player: player} do
       assert StatusCheck.check_win_or_draw(board, player, {2,4}) == nil
       assert StatusCheck.check_win_or_draw(board, player, {3,3}) == nil
     end
 
-    @tag :setup_board_diag_fwd
+    @tag :setup_board_diag_fwd_block
     test "returns player when a player's piece at loc would win diagonally forward", %{board: board, player: player} do
       assert StatusCheck.check_win_or_draw(board, player, {4,4}) == player
     end
 
-    @tag :setup_board_diag_fwd
+    @tag :setup_board_diag_fwd_block
     test "returns nil when a player's piece at loc would not win diagonally forward", %{board: board, player: player} do
       assert StatusCheck.check_win_or_draw(board, player, {3,4}) == nil
     end
@@ -108,7 +145,7 @@ defmodule ConnectFour.StatusCheckTest do
   end
 
   describe "check_lateral/2" do
-    @describetag :setup_board_lat
+    @describetag :setup_board_lat_block
 
     test "returns true when a player's piece at loc would win laterally", %{board: board, player: player} do
       assert StatusCheck.check_lateral(board[player], {6,1})
@@ -125,7 +162,7 @@ defmodule ConnectFour.StatusCheckTest do
   end
 
   describe "check_vertical/2" do
-    @describetag :setup_board_vert
+    @describetag :setup_board_vert_block
 
     test "returns true when a player's piece at loc would win vertically", %{board: board, player: player} do
       assert StatusCheck.check_vertical(board[player], {1,4})
@@ -139,7 +176,7 @@ defmodule ConnectFour.StatusCheckTest do
   end
 
   describe "check_diag_back/2" do
-    @describetag :setup_board_diag_back
+    @describetag :setup_board_diag_back_block
 
     test "returns true when a player's piece at loc would win diagonally back", %{board: board, player: player} do
       assert StatusCheck.check_diag_back(board[player], {1,4})
@@ -153,7 +190,7 @@ defmodule ConnectFour.StatusCheckTest do
   end
 
   describe "check_diag_fwd/2" do
-    @describetag :setup_board_diag_fwd
+    @describetag :setup_board_diag_fwd_block
 
     test "returns true when a player's piece at loc would win diagonally back", %{board: board, player: player} do
       assert StatusCheck.check_diag_fwd(board[player], {4,4})
@@ -170,7 +207,7 @@ defmodule ConnectFour.StatusCheckTest do
       assert StatusCheck.find_open(board, 3) == {3, 1}
     end
 
-    @tag :setup_board_vert
+    @tag :setup_board_vert_block
     test "returns correct column with some pieces in place", %{board: board} do
       assert StatusCheck.find_open(board, 1) == {1, 4}
       assert StatusCheck.find_open(board, 2) == {2, 3}
@@ -191,7 +228,7 @@ defmodule ConnectFour.StatusCheckTest do
   # Private Functions #
   #####################
 
-  defp setup_board_vert(game_id) do
+  defp setup_board_vert_block(game_id) do
     GameServer.drop_piece(game_id, 1)
     GameServer.drop_piece(game_id, 2)
     GameServer.drop_piece(game_id, 1)
@@ -199,7 +236,12 @@ defmodule ConnectFour.StatusCheckTest do
     GameServer.drop_piece(game_id, 1)
   end
 
-  defp setup_board_lat(game_id) do
+  defp setup_board_vert_win(game_id) do
+    setup_board_vert_block(game_id)
+    GameServer.drop_piece(game_id, 2)
+  end
+
+  defp setup_board_lat_block(game_id) do
     GameServer.drop_piece(game_id, 3)
     GameServer.drop_piece(game_id, 3)
     GameServer.drop_piece(game_id, 4)
@@ -207,7 +249,12 @@ defmodule ConnectFour.StatusCheckTest do
     GameServer.drop_piece(game_id, 5)
   end
 
-  defp setup_board_diag_back(game_id) do
+  defp setup_board_lat_win(game_id) do
+    setup_board_lat_block(game_id)
+    GameServer.drop_piece(game_id, 5)
+  end
+
+  defp setup_board_diag_back_block(game_id) do
     GameServer.drop_piece(game_id, 1)
     GameServer.drop_piece(game_id, 1)
     GameServer.drop_piece(game_id, 1)
@@ -221,7 +268,12 @@ defmodule ConnectFour.StatusCheckTest do
     GameServer.drop_piece(game_id, 3)
   end
 
-  defp setup_board_diag_fwd(game_id) do
+  defp setup_board_diag_back_win(game_id) do
+    setup_board_diag_back_block(game_id)
+    GameServer.drop_piece(game_id, 7)
+  end
+
+  defp setup_board_diag_fwd_block(game_id) do
     GameServer.drop_piece(game_id, 4)
     GameServer.drop_piece(game_id, 4)
     GameServer.drop_piece(game_id, 4)
@@ -233,6 +285,11 @@ defmodule ConnectFour.StatusCheckTest do
     GameServer.drop_piece(game_id, 2)
     GameServer.drop_piece(game_id, 7)
     GameServer.drop_piece(game_id, 2)
+  end
+
+  def setup_board_diag_fwd_win(game_id) do
+    setup_board_diag_fwd_block(game_id)
+    GameServer.drop_piece(game_id, 7)
   end
 
   defp setup_board_full(game_id) do
