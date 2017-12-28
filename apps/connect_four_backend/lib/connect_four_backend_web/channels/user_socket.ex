a47c6f105946d3042e9ba807097b3aebf3559b2a
Dave Lively <davelively14@gmail.com>
2017-12-27 22:17:24 -0500

    Heroku configs

--- apps/connect_four_backend/lib/connect_four_backend_web/channels/user_socket.ex
+++ apps/connect_four_backend/lib/connect_four_backend_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ConnectFourBackendWeb.UserSocket do
   # channel "room:*", ConnectFourBackendWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
