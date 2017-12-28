aab1cce2fbe2d81b6f7d52c39823639055bb0d17
Dave Lively <davelively14@gmail.com>
2017-12-27 22:30:11 -0500

    More typose...for db stuff I'm not using

--- apps/connect_four_backend/config/prod.exs
+++ apps/connect_four_backend/config/prod.exs
@@ -23,7 +23,7 @@ config :connect_four_backend, ConnectFourBackendWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :hello, ConnectFourBackendWeb.Repo,
+config :connect_four_backend, ConnectFourBackendWeb.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
